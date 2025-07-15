677ee2b976e6413dfc6aac8dfddf72688a9e8877
Abel <lewheabel16@gmail.com>
2025-07-15 18:58:37 +0100

    Test Amis en hébergement

--- api/bdd.php
+++ api/bdd.php
@@ -1,18 +1,18 @@
 <?php
-// try {
-//     $pdo = new PDO('mysql:host=ballast.proxy.rlwy.net;port=55510;dbname=railway;charset=utf8',
-//     'root', 'MLoGMGGmfEdxkPOMSjbFZxepTAYEpVhs');
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     } catch (PDOException $e) {
-//     echo "Erreur de connexion : " . $e->getMessage();
-//     }
-
-
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=social_network;charset=utf8',
-    'root', '');
+    $pdo = new PDO('mysql:host=ballast.proxy.rlwy.net;port=55510;dbname=railway;charset=utf8',
+    'root', 'MLoGMGGmfEdxkPOMSjbFZxepTAYEpVhs');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
     }
-?>
+
+
+// try {
+//     $pdo = new PDO('mysql:host=127.0.0.1;dbname=social_network;charset=utf8',
+//     'root', '');
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     } catch (PDOException $e) {
+//     echo "Erreur de connexion : " . $e->getMessage();
+//     }
+// ?>
